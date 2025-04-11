70ed571698e9ef5196fdbea811b216accdbdb220
Ashwin Singh <ashwin23156@iiitd.ac.in>
2025-04-11 19:54:38 +0530

    Add complaint functions

--- controllers/ComplaintController.js
+++ controllers/ComplaintController.js
@@ -40,7 +40,66 @@ const fetchActiveComplaints = async (req, res) => {
     }
 };
 
+const fetchDateComplaints = async (req, res) => {
+    const { date } = req.body; 
+
+    try {
+        if (!date) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        const startOfDay = `${date}T00:00:00`;
+        const endOfDay = `${date}T23:59:59.999`;
+
+        const { data, error } = await supabase
+            .from("complaints")
+            .select("*")
+            .gte("complaint_datetime", startOfDay)
+            .lte("complaint_datetime", endOfDay);
+
+        if (error) {
+            return res.status(500).json({ error: "Error fetching complaints" });
+        }
+
+        res.json({ complaints: data });
+    } catch (err) {
+        console.error("Error fetching complaints:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+const fetchUserComplaints = async (req, res) => {
+    const { user_id } = req.body; 
+
+    try {
+        if (!user_id) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        const { data, error } = await supabase
+            .from('files')
+            .select(`
+                complaint_id,
+                is_resolved,
+                complaints (
+                    complaint,
+                    complaint_datetime
+                )
+            `)
+            .eq('user_id', user_id);
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return res.status(500).json({ error: "Error fetching complaints" });
+        }
+
+        res.json({ complaints: data });
+    } catch (err) {
+        console.error("Error fetching complaints:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
 
 
 
-module.exports = { fetchActiveComplaints };
+module.exports = { fetchActiveComplaints, fetchDateComplaints, fetchUserComplaints };
--- routes/ComplaintRoutes.js
+++ routes/ComplaintRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { fetchActiveComplaints } = require("../controllers/ComplaintController");
+const { fetchActiveComplaints, fetchDateComplaints, fetchUserComplaints } = require("../controllers/ComplaintController");
 
 router.get("/active-complaints", fetchActiveComplaints);
+router.post("/date-complaints", fetchDateComplaints);
+router.post("/user-complaints", fetchUserComplaints);
 
 module.exports = router;
