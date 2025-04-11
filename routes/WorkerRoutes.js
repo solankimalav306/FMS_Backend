a2f192df1f55c1cb756fec00ea595d671c71efd5
Malav Mehulkumar Solanki <malav23306@iiitd.ac.in>
2025-04-11 05:31:50 +0530

    Update WorkerRoutes.js

--- routes/WorkerRoutes.js
+++ routes/WorkerRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { loginWorker, fetchWorkQueue, fetchPreviousOrders ,markRequestCompleted,createOrder} = require("../controllers/WorkerController");
+const { loginWorker, fetchWorkQueue, fetchPreviousOrders ,markRequestCompleted,createOrder,getLatestAssignedWorkers} = require("../controllers/WorkerController");
 
 router.post("/login", loginWorker);
 router.post("/work-queue", fetchWorkQueue);
 router.get("/previous-orders", fetchPreviousOrders);
 router.post("/mark-completed", markRequestCompleted);
 router.post("/create-order", createOrder);
+
+router.get("/latest-area-of-service", getLatestAssignedWorkers);
 
 module.exports = router;
