be1cc84b0d7042e689353504d7f5b177a7aabf57
Malav Mehulkumar Solanki <malav23306@iiitd.ac.in>
2025-04-11 18:11:38 +0530

    crash fix

--- routes/StatisticsRouter.js
+++ routes/StatisticsRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getAverageRequestsPerService,getTopRatedWorkersByRole } = require("../controllers/StatisticsController");
+const { getAverageRequestsPerService,getTopRatedWorkersByRole , getUserServiceRequestsByDate} = require("../controllers/StatisticsController");
 
 router.get("/avg-requests-per-service", getAverageRequestsPerService);
 router.get("/top-rated-workers", getTopRatedWorkersByRole);
