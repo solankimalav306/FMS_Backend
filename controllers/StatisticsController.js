5887a31c2d0b7cf3d3f74676baccfdf9cc31026c
Malav Mehulkumar Solanki <malav23306@iiitd.ac.in>
2025-04-11 18:25:49 +0530

    Update StatisticsController.js

--- controllers/StatisticsController.js
+++ controllers/StatisticsController.js
@@ -120,6 +120,7 @@ const getUserServiceRequestsByDate = async (req, res) => {
         if (error) {
             return res.status(500).json({ error: "Database fetch error", details: error.message });
         }
+        
 
         return res.status(200).json({ services: data });
     } catch (err) {
