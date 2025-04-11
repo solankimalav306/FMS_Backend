86859c956ceda928a3c285e00d81be0e0893468d
Ashwin Singh <ashwin23156@iiitd.ac.in>
2025-04-11 23:41:10 +0530

    Update file

--- controllers/ServiceController.js
+++ controllers/ServiceController.js
@@ -15,4 +15,21 @@ const fetchServices = async (req, res) => {
     }
 }
 
-module.exports = { fetchServices };
+const num3 = async (req, res) => {
+    try {
+        const { data: services, error } = await supabase.rpc('get_missing_services_by_building', {
+            building_input: 'R&D block'
+        });
+
+        if (error || !services) {
+            return res.status(401).json({ error: "Error fetching services." });
+        }
+        res.json({ services });
+    } catch (err) {
+        console.error("Data fetch error:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+
+module.exports = { fetchServices, num3 };
